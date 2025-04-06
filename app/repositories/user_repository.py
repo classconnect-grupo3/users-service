0ad9c298ea0ed8c75f21ac7a997c72319329a1ea
tinchomorilla <mmorilla@fi.uba.ar>
2025-04-06 16:28:29 -0300

    delete unused line

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,4 +1,3 @@
-from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from app.models.user_model import User as DBUser
 from app.schemas.user import UserBase
@@ -7,6 +6,7 @@ from app.schemas.user import UserBase
 def get_user(db: Session, name: str, surname: str):
     return db.query(DBUser).filter_by(name=name, surname=surname).first()
 
+
 # Create a user in the database
 def db_create_user(db: Session, user: UserBase):
     db_user = DBUser(name=user.name, surname=user.surname)
@@ -14,8 +14,6 @@ def db_create_user(db: Session, user: UserBase):
     db.commit()
     db.refresh(db_user)
     return db_user
-
-
 
 
 # Get all users from the database
