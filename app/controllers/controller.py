85ffbe69bcb11bd177d9eda5fa460add104fb9c1
tinchomorilla <mmorilla@fi.uba.ar>
2025-04-06 01:21:03 -0300

    Co-authored-by: rafaortegano <rortegano@fi.uba.ar>

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -11,9 +11,11 @@ router = APIRouter()
 # Create a new user (POST request)
 @router.post(
     "/",
+    status_code=201,
     responses={
         201: {"description": "User created successfully", "model": UserResponse},
         400: {"description": "Bad request error", "model": ErrorResponse},
+        409: {"description": "Conflict error - user already exists","model": ErrorResponse},
     },
 )
 def create_user(user: UserBase, db: Session = Depends(get_db)):
