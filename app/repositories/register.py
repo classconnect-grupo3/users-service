b0f06fc09d88be54a418381f70e7abecf3ea6753
tinchomorilla <mmorilla@fi.uba.ar>
2025-04-07 17:21:39 -0300

    delete unnecessary code
    Co-authored-by: rafaortegano <rortegano@fi.uba.ar>

---
+++ app/repositories/register.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import Session
+from app.models.user_model import User as DBUser
+from app.schemas.user import UserBase
+from app.common.security import hash_password 
+
+def get_user(db: Session, email: str):
+    return db.query(DBUser).filter_by(email=email).first()
+
+
+# Create a user in the database
+def db_create_user(db: Session, user: UserBase):
+    hashed_password = hash_password(user.password)
+    db_user = DBUser(
+        name=user.name,
+        surname=user.surname,
+        email=user.email,
+        password=hashed_password,
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
---
+++ app/services/register.py
@@ -0,0 +1,18 @@
+# app/services/user_service.py
+from sqlalchemy.orm import Session
+from app.errors.register_errors import UserAlreadyExists
+from app.repositories.register import get_user, db_create_user
+from app.schemas.user import UserBase
+from app.models.user_model import User
+from app.common.result import Success, Failure, Result
+
+
+def create_new_user(db: Session, user: UserBase) -> Result[User]:
+    existing_user = get_user(db, user.email)
+    if existing_user:
+        return Failure(UserAlreadyExists())
+    new_user = db_create_user(db=db, user=user)
+    return Success(new_user)
+
+
+
