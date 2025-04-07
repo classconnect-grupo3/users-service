27473b59e900b489c6575e25663a40a2f719671c
tinchomorilla <mmorilla@fi.uba.ar>
2025-04-07 19:59:49 -0300

    fix: ensure message attribute is properly initialized in AuthenticationError
    Co-authored-by: rafaortegano <rortegano@fi.uba.ar>

--- app/errors/authentication_errors.py
+++ app/errors/authentication_errors.py
@@ -1,6 +1,7 @@
 class AuthenticationError(Exception):
     def __init__(self, message: str, http_status_code: int):
         super().__init__(message)
+        self.message = message
         self.http_status_code = http_status_code
 
 
--- app/errors/register_errors.py
+++ app/errors/register_errors.py
@@ -1,6 +1,7 @@
 class RegisterUserError(Exception):
     def __init__(self, message: str, http_status_code: int):
         super().__init__(message)
+        self.message = message
         self.http_status_code = http_status_code
 
 
