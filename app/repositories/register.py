a02ec80bf57a7b8feea975de8b01165e3773f8d2
tinchomorilla <mmorilla@fi.uba.ar>
2025-04-13 16:22:38 -0300

    format code
    Co-authored-by: rafaortegano <rortegano@fi.uba.ar>

--- app/repositories/register.py
+++ app/repositories/register.py
@@ -6,11 +6,7 @@ from app.schemas.user import UserBase
 # Create a user in the database
 def db_create_user(db: Session, user: UserBase, uid: str):
     db_user = DBUser(
-        uid=uid,
-        name=user.name,
-        surname=user.surname,
-        email=user.email,
-        location=None
+        uid=uid, name=user.name, surname=user.surname, email=user.email, location=None
     )
     db.add(db_user)
     db.commit()
