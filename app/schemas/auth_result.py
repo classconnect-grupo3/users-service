29aa4ba354d5c602d7743fdd4d6b79ddfd645ce3
tinchomorilla <mmorilla@fi.uba.ar>
2025-04-14 16:44:04 -0300

    delete unnecessary code

--- app/schemas/auth_result.py
+++ app/schemas/auth_result.py
@@ -5,17 +5,3 @@ from pydantic import BaseModel
 class AuthResult(BaseModel):
     id_token: str
     user_location: Optional[str] = None
-
-    class Config:
-        schema_extra = {
-            "examples": [
-                {
-                    "id_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
-                    "user_location": "Argentina",
-                },
-                {
-                    "id_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
-                    "user_location": None,
-                },
-            ],
-        }
