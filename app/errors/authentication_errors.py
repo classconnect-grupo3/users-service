d94edeca2f3c464c4bff2f0fab2618f6aa9356cf
tinchomorilla <mmorilla@fi.uba.ar>
2025-04-13 22:06:54 -0300

    add possible auth errors
    Co-authored-by: rafaortegano <rortegano@fi.uba.ar>

--- app/errors/authentication_errors.py
+++ app/errors/authentication_errors.py
@@ -8,3 +8,27 @@ class AuthenticationError(Exception):
 class InvalidCredentialsError(AuthenticationError):
     def __init__(self, message: str = "Invalid email or password."):
         super().__init__(message, http_status_code=401)
+
+class ExpiredTokenError(AuthenticationError):
+    def __init__(self, message: str = "Token has expired."):
+        super().__init__(message, http_status_code=401)
+
+
+class RevokedTokenError(AuthenticationError):
+    def __init__(self, message: str = "Token has been revoked."):
+        super().__init__(message, http_status_code=401)
+
+
+class InvalidTokenError(AuthenticationError):
+    def __init__(self, message: str = "Invalid token."):
+        super().__init__(message, http_status_code=401)
+
+
+class CertificateFetchError(AuthenticationError):
+    def __init__(self, message: str = "Failed to fetch public certificates for token verification."):
+        super().__init__(message, http_status_code=500)
+
+
+class UIDNotFoundError(AuthenticationError):
+    def __init__(self, message: str = "UID not found in token."):
+        super().__init__(message, http_status_code=400)
