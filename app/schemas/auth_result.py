c3333e417d263d0bc27a37cb245a4266eb7332a6
tinchomorilla <mmorilla@fi.uba.ar>
2025-04-14 16:28:03 -0300

    provide examples of the response model to the client

--- app/schemas/auth_result.py
+++ app/schemas/auth_result.py
@@ -1,8 +1,21 @@
 from typing import Optional
-
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class AuthResult(BaseModel):
     id_token: str
     user_location: Optional[str] = None
+
+    class Config:
+        schema_extra = {
+            "examples": [
+                {
+                    "id_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
+                    "user_location": "Argentina",
+                },
+                {
+                    "id_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
+                    "user_location": None,
+                },
+            ],
+        }
