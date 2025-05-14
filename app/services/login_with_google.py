6423f88f7aed5e67546cc179045cd9422f242cdb
tinchomorilla <mmorilla@fi.uba.ar>
2025-05-14 16:55:11 -0300

    refactor: remove deprecated user creation (in DB) function and update authentication flow

--- app/services/login_with_google.py
+++ app/services/login_with_google.py
@@ -1,7 +1,7 @@
 from app.common.result import Success, Failure
-from app.repositories.login_with_google import create_user_from_google_db
 from firebase_admin import auth as firebase_auth
-from app.common.constants import OK
+from app.common.constants import OK, OLD_USER
+from app.repositories.users import get_user_by_email_db, store_user_in_db
 from sqlalchemy.orm import Session
 from app.errors.google_auth_errors import (
     ArgumentsMissingError,
@@ -32,8 +32,12 @@ def authenticate_with_google(
         first_name = name_parts[0]
         last_name = " ".join(name_parts[1:]) if len(name_parts) > 1 else None
 
-        result = create_user_from_google_db(
-            db,
+        user = get_user_by_email_db(db, email)
+        if user:
+            return Success(OLD_USER)  # Ya estaba registrado
+
+        result = store_user_in_db(
+            db=db,
             uid=uid,
             name=first_name,
             surname=last_name,
@@ -41,7 +45,7 @@ def authenticate_with_google(
         )
 
         if isinstance(result, Failure):
-            return Failure(result.error)
+            return Failure(result)
 
         return Success(result)
 
