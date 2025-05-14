7c9259572a2fb3efe5040d79db5360ccf5a174ac
tinchomorilla <mmorilla@fi.uba.ar>
2025-05-14 16:48:24 -0300

    refactor: streamline user creation process and remove redundant error handling

--- app/controllers/register.py
+++ app/controllers/register.py
@@ -18,16 +18,5 @@ async def create_user(user: UserBase, db: Session = Depends(get_db)):
     result = await create_new_user(db, user)
     if isinstance(result, Failure):
         error = result.error
-        if isinstance(error, UserAlreadyExists):
-            raise HTTPException(
-                status_code=error.http_status_code,
-                detail=error.message,
-                headers={"X-Error": "Conflict"},
-            )
-        if isinstance(error, CouldNotCreateFirebaseUser):
-            raise HTTPException(
-                status_code=error.http_status_code,
-                detail=error.message,
-                headers={"X-Error": "FirebaseError"},
-            )
+        raise HTTPException(status_code=error.http_status_code, detail=error.message)
     return {"data": result.value}
