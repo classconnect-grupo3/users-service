b4fcb670fb9a8d108d8cf479b39192861fe62bd4
tinchomorilla <mmorilla@fi.uba.ar>
2025-05-19 17:09:20 -0300

    add generic error classes to improve the return of possible errors

---
+++ app/errors/generic_errors.py
@@ -0,0 +1,22 @@
+class GenericError(Exception):
+    def __init__(self, message: str, http_status_code: int):
+        super().__init__(message)
+        self.message = message
+        self.http_status_code = http_status_code
+
+
+class UidOrEmailNotProvided(GenericError):
+    def __init__(self, message: str = "Either uid or email must be provided"):
+        super().__init__(message, http_status_code=400)
+
+class UserIsAlreadyAnAdmin(GenericError):
+    def __init__(self, message: str = "User is already an admin"):
+        super().__init__(message, http_status_code=400)
+
+class CouldNotUpdateUser(GenericError):
+    def __init__(self, message: str = "Could not update user"):
+        super().__init__(message, http_status_code=500)
+
+class UserNotProvided(GenericError):
+    def __init__(self, message: str = "User not provided"):
+        super().__init__(message, http_status_code=400)
