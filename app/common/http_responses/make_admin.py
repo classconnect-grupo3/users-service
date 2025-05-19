9b7bafc0f0572b301249155e8e6445d26dbccb5b
tinchomorilla <mmorilla@fi.uba.ar>
2025-05-19 17:52:17 -0300

    fix: update "make_admin_response" to include "UserProfileResponse" model for successful admin promotion

--- app/common/http_responses/make_admin.py
+++ app/common/http_responses/make_admin.py
@@ -1,10 +1,11 @@
 from app.schemas.error_response import ErrorResponse
+from app.schemas.user import UserProfileResponse
 
 
 make_admin_response = {
     201: {
         "description": "User updated to Admin successfully",
-        "model": None,
+        "model": UserProfileResponse,
     },
     400: {"description": "User is already an admin", "model": ErrorResponse},
     404: {
